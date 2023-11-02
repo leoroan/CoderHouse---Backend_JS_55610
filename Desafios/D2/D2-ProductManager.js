64b6fe14bdc7e9f2f9127953377f09122fb6ce8a
Leandro Maselli <leoroan@gmail.com>
2023-11-02 17:47:35 -0300

    working on

--- Desafios/D2/D2-ProductManager.js
+++ Desafios/D2/D2-ProductManager.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+// const path = require("path");
 
 class Product {
   constructor(id, title, description, price, thumbnail, code, stock) {
@@ -14,11 +15,12 @@ class Product {
 
 class ProductManager {
   constructor(ruta, file) {
-    this.path = ruta;
-    this.fileName = path + file;
-    if (fs.existsSync(fileName)) {
+    // this.path = ruta;
+    // this.fileName = path.join(this.path, file);
+    this.fileName = (ruta + file);
+    if (fs.existsSync(this.fileName)) {
       try {
-        let products = fs.readFileSync(fileName, "utf-8");
+        let products = fs.readFileSync(this.fileName, "utf-8");
         this.products = JSON.parse(products);
       } catch (error) {
         this.products = [];
@@ -49,39 +51,87 @@ class ProductManager {
   }
 
   generateUniqueID() {
-    return Math.random().substring(2, 9);;
+    return Math.random().toString(36).substring(2, 9);
   }
 
   async saveProductsToFile(data) {
     try {
       await fs.promises.writeFile(
         this.fileName,
-        JSON.stringify(data)
+        JSON.stringify(data, null, "\t")
       );
-      return true;
+      // return true;
     } catch (error) {
-      throw new Error("something gone wrong saving..", error);
+      throw new Error("something gone wrong saving", error);
     }
   }
 
   getProducts() {
-    return this.products;
+    if (fs.existsSync(this.fileName)) {
+      try {
+        let prods = fs.readFileSync(this.fileName, "utf-8");
+        this.prods = JSON.parse(prods);
+        return this.prods;
+      } catch {
+        throw new Error("something gone wrong retrieving data");
+      }
+    }
+    else return this.products;
   }
 
   getProductById(productId) {
-    if (!productId) {
-      throw new Error('Product ID is required');
+    try {
+      if (typeof productId !== 'number' || isNaN(productId)) {
+        throw new Error('Product ID must be a valid number');
+      }
+      const prods = this.getProducts();
+      const product = prods.find(product => product.id === productId);
+      if (!product) {
+        throw new Error('Product not found');
+      }
+      return product;
+    } catch (error) {
+      throw new Error('Error in getProductById: ' + error.message);
     }
+  }
 
-    if (typeof productId !== 'number') {
-      throw new Error('Product ID must be a number');
+  updateProduct(id, newProduct) {
+    try {
+      const product = this.getProductById(id);
+      if (typeof newProduct === 'object') {
+        for (const key in newProduct) {
+          if (key !== 'id') {
+            if (newProduct.hasOwnProperty(key)) {
+              product[key] = newProduct[key];
+            } else {
+              throw new Error(`Property '${key}' not found in the update data.`);
+            }
+          }
+        }
+        return product;
+      } else {
+        throw new Error('The data provided for update was not an object.');
+      }
+    } catch (error) {
+      throw new Error('Error updating product: ' + error.message);
     }
+  }
 
-    if (!this.products.find(product => product.id === productId)) {
-      throw new Error('Product not found');
+  deleteProduct(productId) {
+    try {
+      const products = this.getProducts();
+      const index = products.findIndex(product => product.id === productId);
+
+      if (index !== -1) {
+        products.splice(index, 1);
+        this.saveProductsToFile(products);
+        return true;
+      } else {
+        throw new Error('Product not found');
+      }
+    } catch (error) {
+      throw new Error('Error deleting product: ' + error.message);
     }
-
-    return this.products.find(product => product.id === productId);
   }
 }
 
@@ -89,4 +139,38 @@ class ProductManager {
 
 // TESTs
 
-const mngr = new ManagerUsuarios("./", "Productos.json");
+// Se creará una instancia de la clase “ProductManager”
+const mngr = new ProductManager("./", "Productos.json");
+
+// Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
+console.log(mngr.getProducts());
+
+// Se llamará al método “addProduct” con los campos:
+// title: “producto prueba”
+// description:”Este es un producto prueba”
+// price:200,
+// thumbnail:”Sin imagen”
+// code:”abc123”,
+// stock:25
+mngr.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+
+// Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
+// El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
+console.log(mngr.getProducts());
+
+// Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.
+//sin error
+console.log(mngr.getProductById(1));
+//con error
+console.log(mngr.getProductById(2));
+
+// // Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.
+// mngr.updateProduct(1, { price: 350 });
+// console.log(mngr.getProducts());
+
+// // Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.
+// mngr.deleteProduct(5);
+// mngr.deleteProduct(1);
+
+
+
