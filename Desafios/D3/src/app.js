1c8c6d378cf85b1081e1acd16f35419dd45e5c4d
Leandro Maselli <leoroan@gmail.com>
2023-11-09 17:34:03 -0300

    completando la task

--- Desafios/D3/src/app.js
+++ Desafios/D3/src/app.js
@@ -1,23 +1,31 @@
-// const express = require("express");
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended:true}))
+
+const productos = require('../../D2/Productos.json');
+
 
 app.get("/", (req, res) => {
   res.send("<h1> Hello World! </h1>");
 });
 
-// Desafio de clase
 app.get("/", (req, res) => {
   res.send("<h1 style='color: blue'>Bienvenido </h1>");
 });
 
-app.get("/usuario", (request, res) => {
+app.get("/usuario", (req, res) => {
   res.json({
-    nombre: "Emiliano",
-    apellido: "Perez",
-    edad: 26,
-    correo: "emi@gmail.com",
+    nombre: "Cosme",
+    apellido: "Fulanito",
+    edad: 62,
+    correo: "cFulano@gmail.com",
   });
 });
+
+app.get("/productos", (req, res) => {
+  res.send(productos);
+});
+
+
 
 app.listen(8080, () => console.log("Server listening on port 8080"));
