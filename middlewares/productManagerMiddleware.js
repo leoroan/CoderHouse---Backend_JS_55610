20f109f8e9fc3616c6af374e70bd90ee420d3469
Leandro <leoroan@gmail.com>
2023-11-16 19:59:53 -0300

    nuevo middleware de limit

--- middlewares/productManagerMiddleware.js
+++ middlewares/productManagerMiddleware.js
@@ -1,8 +1,9 @@
 import ProductManager from "../models/ProductManager.js";
 
 const productManager = new ProductManager("./models/data/", "productos.json");
+const products = productManager.getProducts();
 
-const getProductByIdMiddleware  = (req, res, next) => {
+const getProductByIdMiddleware = (req, res, next) => {
   try {
     const productId = parseInt(req.params.pid);
     const foundProduct = productManager.getProductById(productId);
@@ -13,6 +14,24 @@ const getProductByIdMiddleware  = (req, res, next) => {
   }
 };
 
+const getLimitMiddleware = (req, res, next) => {
+  let { limit } = req.query;
+  try {
+    if (limit) {
+      limit = parseInt(limit);
+      const limitedProducts = products.slice(0, limit);
+      req.products = limitedProducts;
+      next();
+    } else {
+      req.products = products;
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error: ' + error.message });
+  }
+}
+
 export {
-  getProductByIdMiddleware
+  getProductByIdMiddleware,
+  getLimitMiddleware
 }
