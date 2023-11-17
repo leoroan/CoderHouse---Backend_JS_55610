0c6ffb4ce4bb9f10582314e24908bf3065cea534
Leandro <leoroan@gmail.com>
2023-11-17 18:58:05 -0300

    improved router

--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -1,24 +1,28 @@
 import { Router } from "express";
-import { getCartByIdMiddleware, postCartMiddleware, addProductToCartMiddleware } from "../../middlewares/cartManagerMiddleware.js";
-import { getProductByIdMiddleware } from '../../middlewares/productManagerMiddleware.js';
+import {
+  getCartByIdMiddleware,
+  postCartMiddleware,
+  addProductToCartMiddleware
+} from "../../middlewares/cartManagerMiddleware.js";
 
+import { getProductByIdMiddleware } from '../../middlewares/productManagerMiddleware.js';
 
 const router = Router();
 
 // Get the cart by id
 router.get("/:cid", getCartByIdMiddleware, (req, res) => {
   const { cart } = req;
-  res.json({ cart });
+  res.status(200).json({ cart });
 });
 
 // Add to the cart
 router.post("/", postCartMiddleware, (req, res) => {
-  res.json(req.body);
+  res.status(201).json({ message: 'Cart created successfully', cart: req.body });
 });
 
 // Add product to the cart
 router.post("/:cid/product/:pid", getCartByIdMiddleware, getProductByIdMiddleware, addProductToCartMiddleware, (req, res) => {
-  res.json({ message: 'Product added to cart' });
+  res.status(200).json({ message: 'Product added to cart', cart: req.cart });
 });
 
 export default router;
