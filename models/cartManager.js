3b5bbcd11050a9435c90afb87a6e81dd84d5e6e1
Leandro Maselli <leoroan@gmail.com>
2023-11-23 16:00:06 -0300

    improved comments

--- models/cartManager.js
+++ models/cartManager.js
@@ -1,5 +1,6 @@
 import Cart from './Cart.js';
 import fs from 'fs';
+import { uuid } from 'uuidv4';
 
 class CartManager {
   constructor(route, file) {
@@ -7,6 +8,12 @@ class CartManager {
     this.initialize();
   }
 
+  /**
+   * Initialize the cart manager by reading the cart file if it exists.
+   * If the file does not exist, create an empty array to store the carts.
+   * If the file exists but cannot be read, log an error and use an empty array.
+   * @returns {void}
+   */
   initialize() {
     if (fs.existsSync(this.fileName)) {
       try {
@@ -21,6 +28,16 @@ class CartManager {
     }
   }
 
+  /**
+   * Add a new cart to the array of carts.
+   * Generate a unique ID for the new cart.
+   * Create a new Cart object with the generated ID.
+   * Add the new cart to the array of carts.
+   * Save the updated array of carts to the file.
+   * If any error occurs during the process, log it and throw an error.
+   
+   * @returns {void}
+   */
   async addCart() {
     try {
       const id = this.generateUniqueID();
@@ -33,11 +50,23 @@ class CartManager {
     }
   }
 
+  /**
+   * Generate a unique ID for a new cart.
+   * @returns 
+   */
   generateUniqueID() {
-    const maxId = this.carts.reduce((max, cart) => (cart.id > max ? cart.id : max), 0);
-    return maxId + 1;
+    // const maxId = this.carts.reduce((max, cart) => (cart.id > max ? cart.id : max), 0);
+    // return maxId + 1;
+    return uuid();
   }
 
+  /**
+   * Save the carts array to the file.
+   * Convert the carts array to a JSON string.
+   * Write the JSON string to the file.
+   * If any of the file operations fail, log the error and throw an error.
+   * @param {*} data 
+   */
   async saveCartsToFile(data) {
     try {
       await fs.promises.writeFile(
@@ -50,10 +79,23 @@ class CartManager {
     }
   }
 
+  /**
+   * Get all the carts in the array of carts.
+   * @returns 
+   */
   getCarts() {
     return this.carts;
   }
 
+  /**
+   * Get a cart by its ID.
+   * Validate the cart ID.
+   * Check if the cart exists.
+   * If the cart exists, return it.
+   * Otherwise, throw an error.
+   * @param {*} cartId 
+   * @returns 
+   */
   getCartById(cartId) {
     try {
       if (typeof cartId !== 'number' || isNaN(cartId)) {
@@ -70,6 +112,18 @@ class CartManager {
     }
   }
 
+  /**
+   * Update the cart with the given product.
+   * Check if the product exists in the cart.
+   * If the product exists, increment its quantity by 1.
+   * If the product does not exist, add it to the cart with a quantity of 1.
+   * Save the updated cart to the file.
+   * If any error occurs during the process, log it and throw an error.
+   * 
+   * @returns {void}
+   * @param {*} cart 
+   * @param {*} product 
+   */
   async updateCart(cart, product) {
     const existingItem = cart.items.find((item) => item.id === product.id);
     if (existingItem) {
