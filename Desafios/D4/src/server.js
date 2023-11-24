876248742d31d41a67bff80c13ae670802384693
Leandro <leoroan@gmail.com>
2023-11-23 21:26:51 -0300

    added files

--- Desafios/D4/src/server.js
+++ Desafios/D4/src/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 import productsRouter from "./routes/products.router.js";
 import cartRouter from "./routes/cart.router.js";
 
+//D4 imports
+import handlebars from "express-handlebars";
+import __dirname from "./util.js";
+
+
 const PORT = 8080;
 const app = express();
 
@@ -9,11 +14,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Configuramos el engine
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+  })
+);
+
+// Seteamos nuestro motor
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
+
+// Public
+app.use(express.static(`${__dirname}/public`));
+
 // Ruta main
 app.get("/", (req, res) => {
-  res.json({
-    mensaje: "Bienvenido",
-  });
+  res.status(200);
 });
 
 // Routes
