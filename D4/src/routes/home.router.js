f89d7035b113aa9b3bc387fa9269b68dc2b2ed68
Leandro Maselli <leoroan@gmail.com>
2023-11-24 13:41:45 -0300

    updated home router

--- D4/src/routes/home.router.js
+++ D4/src/routes/home.router.js
@@ -1,13 +1,22 @@
 import { Router } from "express";
+import { getLimitMiddleware } from "../../middlewares/productManagerMiddleware.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", (req, res, next) => {
+  req.query.limit = 3;
+  next();
+  
+}, getLimitMiddleware, (req, res) => {
+  const { products } = req;
+
   res.render("home", {
+    fileFavicon: "favicon.ico",
     fileCss: "styles.css",
     fileJs: "scripts.js",
-    title: "Home",
+    title: "Home Shop",
     name: undefined,
+    products: products,
   });
 });
 
