7762496399f2544ef55e66558a1a499ba3c72683
Leandro <leoroan@gmail.com>
2023-12-12 21:23:36 -0300

    message model added

---
+++ models/message.model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const messageSchema = new Schema({
+  sender: {
+    type: String,
+    required: true,
+  },
+  receiver: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const messageModel = model("Message", messageSchema);
+
+export { messageModel };
