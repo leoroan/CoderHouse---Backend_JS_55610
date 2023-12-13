4f42a8e155967c18d3c3cd3b7ddcaf77f4886cfd
Leandro <leoroan@gmail.com>
2023-12-12 21:29:50 -0300

    cart model added

---
+++ models/cart.model.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const cartSchema = new Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  products: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        default: 1,
+      },
+    },
+  ],
+  total: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const cartModel = model("Cart", cartSchema);
+
+export { cartModel };
