95b8d0adcf5ea0ab0c004ecceb7f1f940d1b1893
Leandro Maselli <leoroan@gmail.com>
2023-12-14 10:58:34 -0300

    edit working

--- src/public/script/main.scripts.js
+++ src/public/script/main.scripts.js
@@ -10,10 +10,108 @@ limitSelect.addEventListener("change", function () {
 function showDeleteAlert(button) {
   var productId = button.getAttribute('data-product-id');
 
-  var userConfirmed = confirm('Are you sure you want to delete this item?');
+  var userConfirmed = confirm('Are you sure you want to DELETE this item?');
 
   if (userConfirmed) {
     deleteProduct(productId);
+  }
+}
+
+
+async function editModal(productId) {
+  try {
+    const productData = await getProductById(productId);
+    updateEditModal(productData);
+  } catch (error) {
+    console.error('Error editing product:', error);
+  }
+}
+
+function saveEditedProduct(productId) {
+  const title = document.getElementById('editmodal_title').value;
+  const description = document.getElementById('editmodal_description').value;
+  const price = document.getElementById('editmodal_price').value;
+  const oldPrice = document.getElementById('editmodal_oldPrice').value;
+  const thumbnail = document.getElementById('editmodal_thumbnail').value;
+  const code = document.getElementById('editmodal_code').value;
+  const stock = document.getElementById('editmodal_stock').value;
+  const status = document.getElementById('editmodal_status').value;
+  const category = document.getElementById('editmodal_category').value;
+
+  const productData = {
+    title,
+    description,
+    price,
+    oldPrice,
+    thumbnail,
+    code,
+    stock,
+    status,
+    category,
+  };
+
+  const url = `api/products/${productId}`;
+
+  fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(productData),
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Product updated successfully:', data);
+      location.reload();
+    })
+    .catch(error => {
+      console.error('Error updating product:', error);
+    });
+}
+
+
+function updateEditModal(productData) {
+  document.getElementById('editmodal_title').value = productData.title;
+  document.getElementById('editmodal_description').value = productData.description;
+  document.getElementById('editmodal_price').value = productData.price;
+  document.getElementById('editmodal_oldPrice').value = productData.oldPrice;
+  document.getElementById('editmodal_thumbnail').value = productData.thumbnail;
+  document.getElementById('editmodal_code').value = productData.code;
+  document.getElementById('editmodal_stock').value = productData.stock;
+  document.getElementById('editmodal_status').value = productData.status;
+  document.getElementById('editmodal_category').value = productData.category;
+  var saveButton = document.getElementById("saveEditedProduct");
+
+  saveButton.onclick = function () { saveEditedProduct(productData._id) }
+
+}
+
+async function getProductById(productId) {
+  const url = `api/products/${productId}`;
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+
+    if (response.ok) {
+      const productData = await response.json();
+      return productData;
+    } else {
+      console.error('Error:', response.statusText);
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
   }
 }
 
