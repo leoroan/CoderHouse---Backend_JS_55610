e8f0e65b28626cc4708c1f090096c9b4848f750a
Leandro Maselli <leoroan@gmail.com>
2023-12-18 08:13:20 -0300

    get all, addprods and substract added

--- src/daos/dbManager/cart.dao.js
+++ src/daos/dbManager/cart.dao.js
@@ -1,6 +1,7 @@
 import { cartModel } from "../../model/cart.model.js";
 import ProductDAO from "./product.dao.js";
 
+const pDao = new ProductDAO();
 class CartDao {
   async createCart(id) {
     try {
@@ -19,6 +20,14 @@ class CartDao {
     try {
       const cart = await cartModel.findById(id);
       return cart;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAllCarts() {
+    try {
+      return await cartModel.find();
     } catch (error) {
       throw error;
     }
@@ -45,29 +54,72 @@ class CartDao {
   }
 
   async addProductToCart(cartId, productId, quantity) {
-    try {
-      const cart = await cartModel.findById(cartId);
-      if (!cart) {
-        throw new Error("Cart not found");
-      } 
-      cart.products.push({ productId, quantity });
-      const pDao = new ProductDAO();
-      const product = await pDao.getProductById(productId);
-      if (!product) {
-        throw new Error("Product not found");
-      }
-      const price = product.price;
-      const total = price * quantity;
-      cart.total += total;
-      cart.totalProducts += quantity;
-      cart.updatedAt = new Date();
-      await cart.save();
-
-    } catch (error) {
-      throw error;
+    const cart = await cartModel.findById(cartId);
+    if (!cart) {
+      throw new Error("Cart not found");
     }
+
+    const product = await pDao.getProductById(productId);
+    if (!product) {
+      throw new Error("Product not found");
+    }
+
+    const { price } = product;
+    const total = price * quantity;
+
+    const existingProductIndex = cart.products.findIndex(item => item.productId.equals(productId));
+
+    if (existingProductIndex !== -1) {
+      cart.products[existingProductIndex].quantity += Number(quantity);
+    } else {
+      cart.products.push({ productId, quantity });
+    }
+
+    cart.total += Number(total);
+    cart.totalProducts += Number(quantity);
+    cart.updatedAt = new Date();
+
+    return cart.save();
   }
 
+
+  async deleteProductFromCart(cartId, productId) {
+    try {
+      const productInCart = await cartModel.findOne(
+        {
+          _id: cartId,
+          'products.productId': productId
+        },
+        {
+          'products.$': 1, // get only the matched subdocument
+        });
+
+      if (productInCart) {
+        const result = await cartModel.updateOne(
+          { _id: cartId },
+          { $pull: { products: { productId: productId } } }
+        );
+
+        const product = await pDao.getProductById(productId);
+        const price = product.price;
+        const quantity = productInCart.products[0].quantity;
+        const cart = await cartModel.findById(cartId);
+
+        if (result.acknowledged) {
+          cart.total -= Number(quantity * price);
+          cart.totalProducts -= Number(quantity);
+          cart.updatedAt = new Date();
+
+          console.log('Product removed from cart successfully');
+          return await cart.save();
+        } else {
+          console.error('Product not found in the cart');
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 
