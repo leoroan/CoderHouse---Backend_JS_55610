05240370abacc3db170e1bb87f98ab101927d629
Leandro <leoroan@gmail.com>
2023-12-23 18:33:01 -0300

    changed parameters name

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -4,10 +4,10 @@ import ProductDAO from "../daos/dbManager/product.dao.js";
 const router = Router();
 
 router.get("/", async (req, res, next) => {
-  const { limit } = req.query;
+  const { limit, page, sort, category } = req.query;
   const productDao = new ProductDAO();
-  const products = await productDao.getAllProducts({limit});
-  
+  const products = await productDao.getAllProducts({ limit, page, sort, category });
+
   res.render("index", {
     fileFavicon: "favicon.ico",
     fileCss: "styles.css",
