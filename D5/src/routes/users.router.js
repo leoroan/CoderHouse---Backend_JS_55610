998e93e32f4dca5ac87aa718da76dab2aa9dd3f1
Leandro Maselli <leoroan@gmail.com>
2024-01-10 06:14:02 -0300

    updated

--- D5/src/routes/users.router.js
+++ D5/src/routes/users.router.js
@@ -1,45 +1,13 @@
 import { Router } from "express";
-import UserDao from "../daos/dbManager/user.dao.js";
+import UserDAO from "../daos/dbManager/user.dao.js";
 
 const router = Router();
-const userDao = new UserDao();
-
-router.post('/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-      return res.status(400).json({ error: 'Invalid request parameters' });
-    }
-
-    const user = await userDao.createUser({ username, email, password });
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await userDao.getUserByEmail(email);
-    if (!user || user.password !== password) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-    
-    } else if (user.password === password) {
-      return res.json({ message: 'Login successful' });
-    }
-    res.json(user);
-
-  } catch (error) {
-    console.error(error);
-  }
-});
+const userDao = new UserDAO();
 
 router.get('/profile/:uid', async (req, res) => {
   try {
     const userId = req.params.uid;
-    const userProfile = await userDao.getUserById(userId);
+    const userProfile = await UserDAO.getUserById(userId);
 
     if (!userProfile) {
       return res.status(404).json({ error: 'User not found' });
@@ -54,7 +22,56 @@ router.get('/profile/:uid', async (req, res) => {
   }
 });
 
+router.post('/register', async (req, res) => {
+  try {
+    const { username, email, password, type } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: 'Invalid request parameters' });
+    }
 
+    //Validamos si el user existe en la DB
+    const exist = await UserDAO.getUserByEmail(email);
+    if (exist) {
+      return res.status(400).send({ status: 'error', message: "Usuario ya existe!" })
+    }
 
+    const user = await UserDAO.createUser({ username, email, password, type });
+    res.send({ status: "success", message: "User crated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserDAO.getUserByEmail(email);
+    if (!user || user.password !== password) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+
+    } else if (user.password === password) {
+
+      const userProfileWithoutSensitiveInfo = { ...user.toObject(), password: undefined };
+
+      req.session.user = userProfileWithoutSensitiveInfo;
+      req.session.save();
+
+      return res.json({ message: 'Login successful', payload: req.session.user });
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  req.session.destroy(error => {
+    if (error) {
+      res.json({ error: 'Error logout', msg: "Error al cerrar la session" })
+    }
+    res.send('Session cerrada correctamente!')
+  })
+});
 
 export default router;
