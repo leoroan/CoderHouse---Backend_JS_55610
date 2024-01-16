01edc34043b8587c74299ab89cca86b5a0dd11c2
Leandro Maselli <leoroan@gmail.com>
2024-01-16 10:45:05 -0300

    added bcrypt to register and login for pass hash

--- D6/src/routes/users.router.js
+++ D6/src/routes/users.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import UserDAO from "../daos/dbManager/user.dao.js";
 import CartDao from "../daos/dbManager/cart.dao.js"
+import { createHash, isValidPassword } from "../util.js";
 
 const router = Router();
 const userDao = new UserDAO();
@@ -10,13 +11,10 @@ router.get('/profile/:uid', async (req, res) => {
   try {
     const userId = req.params.uid;
     const userProfile = await UserDAO.getUserById(userId);
-
     if (!userProfile) {
       return res.status(404).json({ error: 'User not found' });
     }
-
     const userProfileWithoutSensitiveInfo = { ...userProfile.toObject(), password: undefined };
-
     res.json({ userProfile: userProfileWithoutSensitiveInfo });
   } catch (error) {
     console.error(error);
@@ -30,15 +28,19 @@ router.post('/register', async (req, res) => {
     if (!username || !email || !password) {
       return res.status(400).json({ error: 'Invalid request parameters' });
     }
-
-    //Validamos si el user existe en la DB
     const exist = await UserDAO.getUserByEmail(email);
     if (exist) {
-      return res.status(400).send({ status: 'error', message: "Usuario ya existe!" })
+      return res.status(400).send({ status: 'error', message: "Usuario existente!" })
+    }
+    const user = {
+      username,
+      email,
+      password: createHash(password),
+      type
     }
 
-    const user = await UserDAO.createUser({ username, email, password, type });
-    await cartDao.createCart(user._id); 
+    const result = await UserDAO.createUser(user);
+    await cartDao.createCart(result._id);
     res.send({ status: "success", message: "User crated successfully" });
   } catch (error) {
     console.error(error);
@@ -50,18 +52,20 @@ router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await UserDAO.getUserByEmail(email);
-    if (!user || user.password !== password) {
-      return res.status(401).json({ error: 'Invalid credentials' });
-
-    } else if (user.password === password) {
-
-      const userProfileWithoutSensitiveInfo = { ...user.toObject(), password: undefined };
-
-      req.session.user = userProfileWithoutSensitiveInfo;
-
-      return res.json({ message: 'Login successful', payload: req.session.user });
+    // if (!user || user.password !== password) {
+    //   return res.status(401).json({ error: 'Invalid credentials' });
+    // } else if (user.password === password) {
+    //   const userProfileWithoutSensitiveInfo = { ...user.toObject(), password: undefined };
+    //   req.session.user = userProfileWithoutSensitiveInfo;
+    //   return res.json({ message: 'Login successful', payload: req.session.user });
+    // }
+    if (!user) return res.status(401).send({ status: 'error', error: "Wrong credentials" })
+    if (!isValidPassword(user, password)) {
+      return res.status(401).send({ status: "error", error: "Incorrect credentials" })
     }
-
+    const userProfileWithoutSensitiveInfo = { ...user.toObject(), password: undefined };
+    req.session.user = userProfileWithoutSensitiveInfo;
+    return res.json({ message: 'Login successful', payload: req.session.user });
   } catch (error) {
     console.error(error);
   }
