d701f7a3324d7cca0e68f6b12645f8ae6df33863
Leandro Maselli <leoroan@gmail.com>
2024-02-06 10:46:56 -0300

    upated router

--- D7/src/routes/products.router.js
+++ D7/src/routes/products.router.js
@@ -11,82 +11,17 @@ const router = Router();
 
 // Get all products
 router.get('/', getAllProductsController);
-// router.get('/', async (req, res) => {
-//   const { limit, page, sort, category } = req.query;
-//   try {
-//     const products = await productDao.getAllProducts({ limit, page, sort, category });
-//     res.status(200).json(products);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
 // Obtener un producto por ID
 router.get('/:id', getProductByIdController)
-// router.get('/:id', async (req, res) => {
-//   const productId = req.params.id;
-
-//   try {
-//     const product = await productDao.getProductById(productId);
-
-//     if (!product) {
-//       return res.status(404).json({ error: 'Producto no encontrado' });
-//     }
-
-//     res.status(200).json(product);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
 // Crear un nuevo producto
 router.post('/', createProductController)
-// router.post('/', async (req, res) => {
-//   const newProductData = req.body;
-
-//   try {
-//     const newProduct = await productDao.addProduct(newProductData);
-//     res.status(201).json(newProduct);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
 // Actualizar un producto por ID
 router.put('/:id', updateProductController)
-// router.put('/:id', async (req, res) => {
-//   const productId = req.params.id;
-//   const updatedProductData = req.body;
-
-//   try {
-//     const updatedProduct = await productDao.updateProduct(productId, updatedProductData);
-
-//     if (!updatedProduct) {
-//       return res.status(404).json({ error: 'Producto no encontrado' });
-//     }
-
-//     res.status(200).json(updatedProduct);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
 // Eliminar un producto por ID
 router.delete('/:id', deleteProductController);
-// router.delete('/:id', async (req, res) => {
-//   const productId = req.params.id;
-
-//   try {
-//     const deletedProduct = await productDao.deleteProduct(productId);
-
-//     if (!deletedProduct) {
-//       return res.status(404).json({ error: 'Producto no encontrado' });
-//     }
-
-//     res.status(204).send(); // No content
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
 export default router;
