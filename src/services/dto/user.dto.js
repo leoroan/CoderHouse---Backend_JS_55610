a1eebac407bc12206eac4bb672e7dfa3b107d614
Leandro Maselli <leoroan@gmail.com>
2024-04-22 13:11:23 -0300

    agregada propiedad para usuariosDto

--- src/services/dto/user.dto.js
+++ src/services/dto/user.dto.js
@@ -1,8 +1,10 @@
 class UserDTO {
-  constructor(username, email, type) {
+  constructor(username, email, type, last_connection) {
     this.username = username;
     this.email = email;
     this.type = type;
+    this.last_connection = new Date(last_connection).toLocaleDateString('es-AR');
+
   }
 }
 
